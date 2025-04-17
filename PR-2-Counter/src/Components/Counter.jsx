8f17b05d821b84fdc1177be5b03037410cbd0e84
Vivek Prajapati <vivekprajapati0405@gmail.com>
2025-04-17 10:10:11 +0530

    PR-2 Counter

---
+++ PR-2-Counter/src/Components/Counter.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import './Counter.css'
+
+
+const Counter = ()=>{
+    const [Count,setCount]= useState(0);
+
+    const Incrimant = ()=>{
+        setCount(Count + 1);
+    }
+
+    const Decrement =()=>{
+       if(Count>0){
+           setCount(Count - 1);   
+       }
+       else{
+        alert("Your Count is 0....... ")
+       }
+    }
+
+    const Reset = ()=>{
+        setCount(0);
+    }
+    return(
+        <div>
+            <h1>Count : {Count}</h1>
+            <button onClick={Decrement}>Decremnat</button>
+            <button className="reset" onClick={Reset}>Reset</button>
+            <button  onClick={Incrimant}>Incrimant</button>
+        </div>
+    )
+}
+
+export default Counter;
+
